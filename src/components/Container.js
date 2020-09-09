3d3622dd8c369eeb097d5e2ac195c5af4220a706
forteshiago <forteshiago@hotmail.com>
2020-09-09 20:59:53 -0300

    Agora vai!

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,41 +1,15 @@
 import React from "react";
 
 import List from "./List";
-import { render } from "react-dom";
 
 
 function Container(){
 
-    function sendNewTask() {
-
-        let task = document.getElementById("newTask").value;
-        let owner = document.getElementById("newOwner").value;
-
-        const fullTask = { task, owner, simpleId: `${Date.now()}`};
-        console.log(fullTask);
-
-        let confirm = true
-        console.log(confirm);
-        render(<List vai={confirm} fullTask={fullTask}/>, document.getElementById("lista"));
-    }
-
     return (
         <div id="container">  
-            <input id="newTask" placeholder="Digite uma nova tarefa" type="text" />
-            <br />
-            <input id="newOwner" placeholder="Digite seu nome" type="text" />
-            <br />
-            <button type="button" onClick={sendNewTask}>
-                Gravar
-            </button>
-            <br />
             
-            <div className="previewTask">
-                <h3>Teste Mensagem</h3>
-                <h3>Teste Dono</h3>
-            </div>
             
-            <List vai={false}/>
+            <List />
 
         </div>
     )
--- src/components/List.js
+++ src/components/List.js
@@ -1,43 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 
-function List({vai, fullTask}){
+function List({}){
+    const [task, setTask] = useState("");
+    const [owner, setOwner] = useState("");
+    const [tasks, setTasks] = useState([]);
 
-    let tasks = [
-        {task:"Estudar React", owner:"Luigi", simpleId:"testeUm1010"},
-        {task:"Refazer os exercícios", owner:"Mario", simpleId:"testeUm1012"}
-    ];
-
-    function renderAllTasks() {
-        return(
-        <div id="lista">
-            <ul>
-            { tasks.map((eachTask) => (
-                <li key={eachTask.simpleId}>
-                    <b>{ eachTask.owner }: </b>{ eachTask.task }
-                </li>            
-            ))}
-            </ul>
-        </div>
-        )
-    }
-
-// ----------------------------------------------------------------------------------
-
-    if(vai == false) {
-        return (
-            renderAllTasks()
-        )
-
-    } else {
-
-        let task = { task:`${fullTask.task}`, owner:`${fullTask.owner}`, simpleId:`${fullTask.simpleId}` };
+    function handleAddScrap() {
+        const newTask = { task, owner, simpleId: `${Date.now()}`};
         
-        tasks.push(task);
-
-        return(
-            renderAllTasks()
-        )
+        setTasks([...tasks, newTask]);
+        setOwner("");
+        setTask("");
+        console.log(newTask);
     }
+
+    function handleDelete (idTask) {
+        setTasks(tasks.filter((eachTask) => eachTask.simpleId !== idTask))
+    }
+
+
+
+    return (
+        <>
+            <input id="newTask" value={task} onChange={(e) => setTask(e.target.value)} placeholder="Digite uma nova tarefa" type="text" />
+            <br />
+            <input id="newOwner" value={owner} onChange={(e) => setOwner(e.target.value)} placeholder="Digite seu nome" type="text" />
+            <br />
+            <button type="button" onClick={handleAddScrap} >
+                Gravar
+            </button>
+
+            <br />
+            
+            <div className="previewTask">
+                <h3>{task}</h3>
+                <h3>{owner}</h3>
+            </div>
+
+            <br />
+
+            <ul>
+                {tasks.map((eachTask) => (
+                    <li key={eachTask.simpleId}>
+                        <b>{eachTask.owner} : </b>
+                        {eachTask.task}
+                        <button type="button" onClick={() => handleDelete(eachTask.simpleId)} > Deletar </button>
+                    </li>
+                ))}
+            </ul>
+
+        </>
+    )
 };
 
 export default List;
